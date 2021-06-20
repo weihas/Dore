9d479e19dcd8dc4ed1b7a5866dc53d664cc649b9
weiha <weihas@icloud.com>
2021-06-20 20:14:33 +0800

    Huge Enhance

---
+++ Dore/View/SettingView/ThemeSettingView.swift
@@ -0,0 +1,72 @@
+//
+//  ThemeSettingView.swift
+//  Dore
+//
+//  Created by WeIHa'S on 2021/6/20.
+//
+
+import SwiftUI
+
+struct ThemeSettingsView: View {
+    @AppStorage("themeSetting") private var themeSetting : Bool = true
+    var body: some View {
+        VStack{
+            Picker(selection: $themeSetting , label: Label("Theme", systemImage: "paintbrush")) {
+                Text("Capsule").tag(true)
+                Text("Circle").tag(false)
+            }
+            HStack{
+                Spacer()
+                VStack{
+                    capsule()
+                        .frame(width: 50, height: 100, alignment: .center)
+                    Text("Capsule")
+                }
+                Spacer()
+                Spacer()
+                VStack{
+                    circle()
+                        .frame(width: 100, height: 100, alignment: .center)
+                    Text("Circle")
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .padding()
+ 
+    }
+    
+    @ViewBuilder
+    func capsule() -> some View {
+        ZStack {
+            Capsule()
+                .frame(alignment: .center)
+                .foregroundColor(.white)
+                .opacity(0.4)
+            Capsule()
+                .clipShape(RectBand(value: 0.4))
+                .animation(.easeOut)
+                .frame(alignment: .center)
+                .foregroundColor(.blue)
+        }
+    }
+    
+    @ViewBuilder
+    func circle() -> some View {
+        GeometryReader { geometry in
+            ZStack{
+                let thickness = geometry.size.height * 0.2
+                CircleShape()
+                    .stroke(Color.white ,style: StrokeStyle(lineWidth: thickness , lineCap: .round, lineJoin: .round))
+                    .opacity(0.4)
+                CircleShape(value: 0.4)
+                    .stroke(Color.blue ,style: StrokeStyle(lineWidth: thickness, lineCap: .round, lineJoin: .round))
+                    .animation(.easeInOut)
+            }
+            .padding()
+        }
+    }
+    
+    
+}
